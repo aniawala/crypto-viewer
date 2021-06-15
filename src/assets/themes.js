942ae98b6312e59eb24f2fccd9db240edca74b7f
Ania Walaszek <64088137+aniawala@users.noreply.github.com>
2021-06-15 22:20:14 +0200

    Implement base app elements using Material UI

---
+++ src/assets/themes.js
@@ -0,0 +1,6 @@
+import "@fontsource/handlee";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  typography: { fontFamily: ["Handlee", "cursive"].join(",") },
+});

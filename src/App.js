fd1163851f525ac21499d32ad9eec4b62ea45ffb
aniawala <anna.walaszek97@gmail.com>
2021-06-15 22:19:20 +0200

    Implement base app elements using Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { ThemeProvider } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { theme } from "./assets/themes";
+import Menu from "./components/Menu";
+import Panel from "./components/Panel";
+
+const useStyles = makeStyles({
+  root: {
+    height: "100%",
+  },
+});
+
 const App = () => {
-  return <div className="app">Hello from app!</div>;
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        className={classes.root}
+        direction="column"
+        justify="space-evenly"
+        alignItems="center"
+      >
+        <Typography variant="h2">Welcome to crypto viewer!</Typography>
+        <Menu />
+        <Panel />
+      </Grid>
+    </ThemeProvider>
+  );
 };
 
 export default App;

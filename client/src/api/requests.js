891d9c54cf400cc5582e9f352859ea05de3c5307
Ania Walaszek <64088137+aniawala@users.noreply.github.com>
2021-06-17 13:54:32 +0200

    Implement filters functionality

--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -1,8 +1,8 @@
 import api from "./index";
 
-export const getCryptocurrencies = async () => {
+export const getCryptocurrencies = async (params) => {
   try {
-    const response = await api.get("/cryptocurrencies");
+    const response = await api.get("/cryptocurrencies", { params });
     return [response.data, null];
   } catch (e) {
     const error = e.response.data.message;
